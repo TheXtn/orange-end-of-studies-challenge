23136eba503d6d415fa58f89afa4fae987ff3c25
FirasKhadraoui <firas.khadraoui@esprit.tn>
2022-12-14 12:12:20 +0100

    Config DB + UserModel

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+// import { useHistory } from 'react-router-dom'
 import './App.css';
 
 function App() {
+  // const history = useHistory()
+  const [name,setName]= useState('')
+  const [email,setEmail]= useState('')
+  const [password,setPassword]= useState('')
+
+  async function registerUser(event){
+    event.preventDefault()
+    const response = await fetch('http://localhost:8000/api/register',{
+      method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name,
+				email,
+				password,
+			}),
+    })
+    const data = await response.json()
+
+		if (data.status === 'ok') {
+			// history.push('/login')
+      console.log("okk")
+		}
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Register</h1>
+        <form onSubmit={registerUser}>
+          <input 
+          type="text" 
+          placeholder=" Name"
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+          />
+          <br/><br/>
+          <input 
+          type="email" 
+          placeholder="Email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          />
+          <br/><br/>
+          <input 
+          type="password" 
+          placeholder="Password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+          />
+          <br/><br/>
+          <input type="submit" value="Register"/>
+        </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config();
+// const User = require ('./src/models/UserModel')
 
-app.get('/hello',(req,res)=>{
-    res.send('hello world')
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log('DB CONNECTED')).catch(err=>console.log(err.message))
+
+app.post('/api/register', async (req, res) => {
+	console.log(req.body)
+	try {
+		// const newPassword = await bcrypt.hash(req.body.password, 10)
+		await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: newPassword,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: 'Duplicate email' })
+	}
 })
 
 app.listen(8000,()=>
