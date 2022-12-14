241dcaa0b089f11b601ccbd5f7f48cb709d2a2c0
TheXtn <52961281+TheXtn@users.noreply.github.com>
2022-12-14 15:55:02 +0100

    Removing usless files

--- orange-end-of-studies-challenge/lib/auth.js
+++
@@ -1,9 +0,0 @@
-import {hash,compare} from 'bcryptjs'
-export async function hashpassword(password){
-    const hashed=await hash(password,12)
-    return hashed
-}
-export async function verif(password,hashpassword){
-    const haja=await compare(password,hashpassword)
-    return haja
-}
--- orange-end-of-studies-challenge/lib/db.js
+++
@@ -1,5 +0,0 @@
-import {MongoClient} from 'mongodb'
-export async function ConnectToDb(){
- const client=await MongoClient.connect(process.env.MONGOURL)
-    return client
-}
