da948ae456299540482143fc4e0646bcff6657b0
TheXtn <52961281+TheXtn@users.noreply.github.com>
2022-12-14 23:44:46 +0100

    ..

--- stockmng/pages/login.js
+++ stockmng/pages/login.js
@@ -1,8 +1,7 @@
 import LoginComp from "../components/Login";
 import Loading from "../components/UI/loading";
 
-import LoadingMask from "react-loadingmask";
-import "react-loadingmask/dist/react-loadingmask.css";
+
 
 export default function Home() {
   return(
--- stockmng/pages/register.js
+++ stockmng/pages/register.js
@@ -1,5 +1,4 @@
 
-import "react-loadingmask/dist/react-loadingmask.css";
 import SignUpComp from "../components/signup";
 
 export default function Reg() {
